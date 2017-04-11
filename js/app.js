7a7f4eafb4378ea3826a8d47d99c8ad39e23219f
Zak <zakattack26111@gmail.com>
2017-04-11 11:00:22 -1000

    Sort Overlay for Homepage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log ("Sanity Check");
+
+//SORT OVERLAY
+function openOverlay(){
+  var mostRecentHeight = document.getElementById("mostRecent").offsetHeight;
+  //console.log(mostRecentHeight);
+  document.getElementById("sortOverlay").style.height = mostRecentHeight + "px";
+	document.getElementById("sortOverlay").style.width = "250px";
+}
+
+function closeOverlay(){
+	document.getElementById("sortOverlay").style.width = "0";
+}
