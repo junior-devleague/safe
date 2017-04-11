44572d786bd43dc39cabce7f52251bfe75a8585d
Christian <christianbuco17@gmail.com>
2017-04-10 21:18:26 -1000

    Drop down with css/js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log ("Sanity Check");
+
+
+
+
+/*Dropdown menu for sort*/
+
+function showDropdownMenu(){
+	document.getElementById("myDropdown").classList.toggle("showSort")
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('#sortOverlay')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('showSort')) {
+        openDropdown.classList.remove('showSort');
+      }
+    }
+  }
+}
