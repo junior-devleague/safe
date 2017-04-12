331c96af5ad8d9db34082daca8151ed66635a99e
Student <idk123@gmail.com>
2017-04-12 09:06:45 -1000

    JewmariZedoriikuPROGRESSSOFAR2

--- js/app.js
+++ js/app.js
@@ -26,3 +26,28 @@ function closeOverlay(){
 
 //Single Topic View
 
+var expand = function() {
+	var wrapHeight = document.getElementByClass(".topic .description").height();
+	var descHeight = document.getElementByClass(".topic").height();
+	if (wrapHeight <= descHeight) {
+		document.getElementByClass(".topic")
+	}
+
+}
+
+/*Turn this code from JQuery to Javascript:
+$(function(){
+    var wrapHeight = $(".product-description .desc-wrap").height();
+    var descHeight = $(".product-description").height();
+    if (wrapHeight <= descHeight) {
+        $(".product-description .desc-fade").hide();
+        $(".product-description .open-link").hide();
+    }
+    
+    $(".product-description .open-link").click(function() {
+        $(this).hide();
+        $(".product-description .desc-fade").hide();
+        $(".product-description").animate({height: wrapHeight}, 1000);
+ 
+$ stands for 
+ */
