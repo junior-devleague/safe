d0976249a815a97c4bdacfc2b57908bef306721d
Student <idk123@gmail.com>
2017-04-12 08:24:23 -1000

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 /*fade in and fade out for view topic */
+
+
+/*Turn this code from JQuery to Javascript:
+$(function(){
+    var wrapHeight = $(".product-description .desc-wrap").height();
+    var descHeight = $(".product-description").height();
+
+    if (wrapHeight <= descHeight) {
+        $(".product-description .desc-fade").hide();
+        $(".product-description .open-link").hide();
+    }
+    
+    $(".product-description .open-link").click(function() {
+        $(this).hide();
+        $(".product-description .desc-fade").hide();
+        $(".product-description").animate({height: wrapHeight}, 1000);
+ */
