3282a3aa8f470a4c5a172d585434126740d9761a
Christian Buco <christianbuco17@gmail.com>
2017-04-28 09:13:21 -1000

    vote almost

--- js/app.js
+++ js/app.js
@@ -65,57 +65,47 @@ target the Id and change the innerHTML.*/
  check if its equal to the number of downvote
   then fade out if it exceed the number*/
 //////////////////////////////////////////////////////
-/*Problems may occur:
-    one like/dislike per computer
-*/
-var dislike = 0;
-var likes = 1;
 
-for (var i = 0; i <= document.getElementsByClassName("numberUp").length - 1; i++) {
-  document.getElementsByClassName("numberUp")[i].addEventListener("click", function(){voteUpPost(this)},{once:true})
-  document.getElementsByClassName("numberDown")[i].addEventListener("click", function(){voteUpPost(this)},{once:true})
-  console.log(i)
-}
 //MAKE ONE FUNCTION
 // If img clicked remove event listener
-  var clicks = 0;
+
+var click = 0;
 function voteUpPost(z){
-  var idOfSpanUp = z.previousSibling.previousSibling.firstChild.nextSibling;
-  // var idOfSpanDown = z.parentElement.parentElement.lastChild.previousSibling.firstChild.nextSibling.lastChild
-  // var imgDownArrow = z.parentElement.parentElement.lastChild.previousSibling.lastChild.previousSibling
-  var imgUpArrow = z
-  clicks++;
-  console.log(z.parentElement.getElementsByTagName('IMG')[0])
-  console.log(z)
-    idOfSpanUp.innerHTML = +idOfSpanUp.innerHTML + 1
-    // console.log
-    // idOfSpanUp.innerHTML = +idOfSpanUp.innerHTML + 1
-    // idOfSpanDown.innerHTML = +idOfSpanDown.innerHTML - 1
-    // imgDownArrow.addEventListener("click",function(){voteDownPost(this)},{once:true})
-    // clicks = 1
+  var idOfSpan = z.previousSibling.previousSibling.firstChild.nextSibling;
+  // console.log(z.parentElement.parentElement.firstChild.nextSibling.firstChild.nextSibling.lastChild)
+  // console.log(z.className)
+  //console.log(z.parentElement.getElementsByTagName('IMG')[0])
+  	// z.addEventListener("click", function(){voteUpPost(this)},{once:true})
+  click++
+  if (z.className == 'numberUp'){
+  	var spanD1 = z.parentElement.parentElement.lastChild.previousSibling.firstChild.nextSibling.firstChild.nextSibling
+  	idOfSpan.innerHTML =  +idOfSpan.innerHTML + 1
+  	if(click == 2){
+  		spanD1.innerHTML = +spanD1.innerHTML - 1
+  		click = 1
+  	}
+  }else if (z.className == 'numberDown'){
+  	var spanU1 = z.parentElement.parentElement.firstChild.nextSibling.firstChild.nextSibling.lastChild
+  	idOfSpan.innerHTML = +idOfSpan.innerHTML + 1
+  	if (click == 2) {
+  		spanU1.innerHTML = +spanU1.innerHTML - 1
+  		click = 1
+  	}
+  }
 }
 
-// function voteDownPost(y){
-//   var idOfSpanUp = y.parentElement.parentElement.firstChild.nextSibling.firstChild.nextSibling.lastChild
-//   var idOfSpanDown = y.parentElement.parentElement.lastChild.previousSibling.firstChild.nextSibling.lastChild
-//   var imgDownArrow = y
-//   var imgUpArrow = y.parentElement.parentElement.firstChild.nextSibling.lastChild.previousSibling
-//   // var clicks = 0
-//   console.log(y.parentElement.parentElement.firstChild.nextSibling.lastChild.previousSibling)
+// function changeVote(y){
+// 	var idOfSpan = y.previousSibling.previousSibling.firstChild.nextSibling;
 
-//   if (clicks == 1) {
-//     idOfSpanDown.innerHTML = +idOfSpanDown.innerHTML + 1
-//   }else if(clicks == 2){
-//     idOfSpanDown.innerHTML = +idOfSpanDown.innerHTML + 1
-//     idOfSpanUp.innerHTML = +idOfSpanUp.innerHTML - 1
-//     imgUpArrow.addEventListener("click",function(){voteUpPost(this)},{once:true})
-//     clicks = 1
-//   }
+// 	idOfSpan.innerHTML = +idOfSpan.innerHTML + 1
 // }
-function test(){
-  var upArrow = document.getElementsByClassName("numberUp");
-  var downArrow = document.getElementsByClassName("numberDown");
-  likes++
-  document.getElementsByClassName('numberUp').innerHTML = "+" + likes
 
+for (var i = 0; i < document.getElementsByClassName("numberUp").length; i++) {
+  document.getElementsByClassName("numberUp")[i].addEventListener("click", function(){
+  	voteUpPost(this)
+  }, {once:true})
+
+  document.getElementsByClassName("numberDown")[i].addEventListener("click", function(){
+  	voteUpPost(this)
+  }, {once:true})
 }
